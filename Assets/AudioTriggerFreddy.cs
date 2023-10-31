7081805aa7a296f78f7d8ed2cfb4ffb51107b170
matthew allison restin <144450180+maddycode15@users.noreply.github.com>
2023-10-31 11:58:24 -0300

    first commit

---
+++ Assets/AudioTriggerFreddy.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTriggerFreddy : MonoBehaviour
+{
+    AudioSource audio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        audio.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
